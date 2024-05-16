aa1769a594965a73b2abdd768a70cf2f6664dc23
walterdiaz1993 <waltermisael1108@gmail.com>
2024-05-15 19:28:45 -0600

    cambios es la entidad

--- Services.NetCore.Domain/Core/BaseEntity.cs
+++ Services.NetCore.Domain/Core/BaseEntity.cs
@@ -15,24 +15,15 @@ namespace Services.NetCore.Domain.Core
 
         public int Id { get; set; }
         public int UId { get; set; }
-        public Guid TransactionUId { get; set; }
 
-        [StringLength(shortVarcharLength)]
-        public string TransactionType { get; set; }
-        public DateTime TransactionDateUtc { get; set; }
         public DateTime? TransactionDate { get; set; }
         public DateTime? CreationDate { get; set; }
 
         [StringLength(shortVarcharLength)]
         public string ModifiedBy { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
         public bool IsActive { get; set; } = true;
 
-        [StringLength(50)]
-        public string TransactionDescription { get; set; }
 
         public BaseEntity()
         {
@@ -41,9 +32,6 @@ namespace Services.NetCore.Domain.Core
         public BaseEntity(string modifiedBy, string transactionType)
         {
             ModifiedBy = modifiedBy;
-            TransactionType = transactionType;
-            TransactionDateUtc = DateTime.UtcNow;
-            TransactionUId = Guid.NewGuid();
         }
 
         [NotMapped]
